8342fa7847bcb351a57403632c7d66d3d9c82530
Michael McLoughlin <mmcloughlin@gmail.com>
2017-01-29 16:37:53 -0800

    doc for log pkg

--- log/base.go
+++ log/base.go
@@ -3,9 +3,12 @@ package log
 
 import "github.com/inconshreveable/log15"
 
+// Logger is the base interface for logging in the pearl packages.
 type Logger interface {
+	// With adds key value pair(s) to the logging context.
 	With(ctx ...interface{}) Logger
 
+	// Logging at levels used in the official Tor client.
 	Debug(msg string, ctx ...interface{})
 	Info(msg string, ctx ...interface{})
 	Notice(msg string, ctx ...interface{})
@@ -27,6 +30,7 @@ func (l log15Adaptor) Notice(msg string, ctx ...interface{}) {
 	l.Info(msg, ctx...)
 }
 
+// NewDebug builds a logger intended for debugging purposes.
 func NewDebug() Logger {
 	return log15Adaptor{
 		Logger: log15.New(),
