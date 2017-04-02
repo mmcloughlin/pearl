a6b9ee538792078d2a2aee87e1013b5fa3118cb7
Michael McLoughlin <mmcloughlin@gmail.com>
2017-04-02 15:37:27 -0700

    basic command to launch router

--- cmd/pearl/cmd/run.go
+++ cmd/pearl/cmd/run.go
@@ -3,6 +3,9 @@ package cmd
 import (
 	"fmt"
 
+	"github.com/mmcloughlin/pearl"
+	"github.com/mmcloughlin/pearl/log"
+	"github.com/mmcloughlin/pearl/torconfig"
 	"github.com/spf13/cobra"
 )
 
@@ -10,11 +13,48 @@ import (
 var runCmd = &cobra.Command{
 	Use:   "run",
 	Short: "Run relay",
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("Hello!")
+	RunE: func(cmd *cobra.Command, args []string) error {
+		return run()
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(runCmd)
+}
+
+func run() error {
+	platform := torconfig.NewPlatformHostOS("pearl", "0.1")
+	config := &torconfig.Config{
+		Nickname: "pearl",
+		ORPort:   9001,
+		Platform: platform.String(),
+	}
+
+	logger := log.NewDebug()
+
+	r, err := pearl.NewRouter(config, logger)
+	if err != nil {
+		return err
+	}
+
+	desc := r.Descriptor()
+	doc, err := desc.Document()
+	if err != nil {
+		return err
+	}
+
+	fmt.Println(string(doc.Encode()))
+
+	go func() {
+		r.Run()
+	}()
+
+	authority := "127.0.0.1:7000"
+	err = desc.PublishToAuthority(authority)
+	if err != nil {
+		return err
+	}
+	logger.With("authority", authority).Info("published descriptor")
+
+	select {}
 }
