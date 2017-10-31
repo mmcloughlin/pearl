00da5e1b798b946eee0e7f65839a21d8606f539f
Michael McLoughlin <mmcloughlin@gmail.com>
2017-10-31 13:14:30 -0700

    add pprof to telemetry

--- telemetry/core.go
+++ telemetry/core.go
@@ -4,6 +4,7 @@ package telemetry
 import (
 	"expvar"
 	"net/http"
+	"net/http/pprof"
 
 	"github.com/mmcloughlin/pearl/log"
 )
@@ -12,6 +13,11 @@ import (
 func Handler() http.Handler {
 	mux := http.NewServeMux()
 	mux.Handle("/debug/vars", expvar.Handler())
+	mux.HandleFunc("/debug/pprof/", pprof.Index)
+	mux.HandleFunc("/debug/pprof/cmdline", pprof.Cmdline)
+	mux.HandleFunc("/debug/pprof/profile", pprof.Profile)
+	mux.HandleFunc("/debug/pprof/symbol", pprof.Symbol)
+	mux.HandleFunc("/debug/pprof/trace", pprof.Trace)
 	return mux
 }
 
